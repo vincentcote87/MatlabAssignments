63ae30293655de5a798ff879c0403a60ff13ff0d
Vincent <vincentcote87@gmail.com>
2019-09-25 21:02:52 -0600

    Finished assn 2

--- homework2_VincentCote.m
+++ homework2_VincentCote.m
@@ -15,3 +15,39 @@ disp(arr);
 
 %Question 5
 disp("Question 5");
+indices = find(arr>5);
+disp(indices);
+
+%Question 6
+disp("Question 6 - See figure");
+x = 0:0.1:10 * pi;
+figure;
+subplot(3,1,1); % using subplot as 3 questions require different figures
+plot(x, cos(x));
+xlabel('Plot of cosine');
+
+%Question 7
+disp('Question 7 - See figure');
+text((5 * pi), 0, 'it is cosine', 'color', 'blue', 'FontSize', 11, 'HorizontalAlignment', 'center');
+
+%Question 8
+disp('Question 8 - See figure');
+subplot(3,1,2); % using subplot as 3 questions require different figures
+histogram(cos(x), 18);
+xlabel('Histogram of cosine');
+
+%Question 9
+disp('Question 9 - See figure');
+subplot(3,1,3); % using subplot as 3 questions require different figures
+histogram(arr, 1:20);
+xlabel('Histogram of arr');
+
+%Question 10
+disp('Question 10');
+[m,n] = size(arr);
+for i = 1:m*n
+    if arr(i) > 5
+        str = ['At index ' num2str(i) ', ' num2str(arr(i)) ' is greater than 5'];
+        disp(str);
+    end
+end
