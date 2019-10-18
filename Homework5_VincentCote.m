79ff1a79430fba3665ba3a057fad24878761ab31
Vincent <vincentcote87@gmail.com>
2019-10-18 15:47:28 -0600

    Finish assn 5

--- Homework5_VincentCote.m
+++ Homework5_VincentCote.m
@@ -1,27 +1,48 @@
-err = 0.1; %-- error margin
-figure; hold on;
-xlim ([0 2]);
-ylim ([0 2]);
+clc;
+clear;
+
+figure;
+hold on;
+axis([0 10 0 10]);
+
+%Question 1
+shortestTime = 1000;
+try
+    highScore = load('highScore.mat');
+    shortestTime = highScore.time;
+    disp('High Score');
+    disp(highScore.playerName);
+    disp(['Time: ' num2str(shortestTime)]);
+    disp(['Accuracy: ' num2str(highScore.accuracy) '%']); 
+catch
+    disp('No high score yet, make it count!');
+end
+
+hit = true;
+score = 0;
+err = 0.1;
+numOfTargets = 6;
 targetSize = 10; %Question 3
+shots = 0;
 penality = 0; %Question 6
-score = 0; %-- number of correct hits
-hit = true; %- will change to 'false' if target is not hit
-tic
-while score < 6 %-- repeat loop until number of correct hits is 6 
+playerName = input('Please enter your name: ', 's');
+
+tic;
+while score < numOfTargets
     if hit
-        x=rand(1); %- location of target
-        y=rand(1);
-        plot(x,y,'o', 'MarkerSize', targetSize, 'Color', [1,0,0]);
+        [x,y] = getPoints();
+        plot(x, y, 'o','MarkerSize', targetSize, 'Color', [1,0,0]);
         hit = false;
         penality = 0;
         for i = 0:score/2 %Question 5
-            plot(rand(1),rand(1),'*', 'MarkerSize', 10, 'Color', [0.5,0.75,1]);
+            [a,b] = getPoints();
+            plot(a, b,'*', 'MarkerSize', 10, 'Color', [0.5,0.75,1]);
         end
     end
-    [x1 y1] = ginput(1); %- location of click
-    plot(x1,y1, '+');
-    dist = sqrt((x-x1)^2+(y-y1)^2); %-- distance between target and click 
-    if  dist< err
+    [user_x, user_y] = ginput(1);
+    shots = shots + 1;
+    plot(user_x, user_y, '+');
+    if sqrt((x-user_x)^2 + (y-user_y)^2) < (err * targetSize/4)
         hit = true;
         pause(penality);
         score = score + 1;
@@ -30,20 +51,36 @@ while score < 6 %-- repeat loop until number of correct hits is 6
         penality = penality + 1;
     end
 end
-
 time = toc;
 title(getMedal(time));
 
+%Question 4
+accuracy = (numOfTargets/shots)*100;
+disp(['Your shots hit ' num2str(accuracy) '% of the time']);
+
+%Only save if it beats the high score, no need to keep all other previous
+%games
+if time < shortestTime
+    save('highScore.mat', 'time', 'playerName', 'accuracy');
+    disp(['Congratulations ' playerName ', you have the new best time!']);
+end
+
 %Question 2
 function str = getMedal(time)
-    if time < 6
+    if time < 7
         medal = 'Gold';
-    elseif time < 9
-        medal = 'Silver'
-    elseif time < 12
-        medal = 'Bronze'
+    elseif time < 10
+        medal = 'Silver';
+    elseif time < 13
+        medal = 'Bronze';
     else
-        medal = ''
+        medal = '';
     end
         str = [num2str(time) ' ' medal];
+end
+
+function [x,y] = getPoints()
+    %*8 + 1 so that it is never on the boundries or axies 
+    x = rand(1) * 8 + 1;
+    y = rand(1) * 8 + 1;
 end
